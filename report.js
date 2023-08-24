e100a00f7bf548b9612ad03cbe70e813fb46459d
MariuszMalankiewicz <m2ario123@wp.pl>
2023-08-24 21:35:34 +0200

    update on oop

--- report.js
+++ report.js
@@ -1,33 +1,87 @@
-const btnGuest = document.getElementsByTagName('button')[0];
-const btnAdmin = document.getElementsByTagName('button')[1];
-let logIn = document.getElementsByTagName('p')[0];
-
-let admin = false;
-
-btnGuest.addEventListener('click', ()=>{
-
-    logIn.textContent = 'you are logged in as: ' + btnGuest.value;
-
-    admin = false;
-})
-
-btnAdmin.addEventListener('click', ()=>{
-
-    logIn.textContent = 'you are logged in as: ' + btnAdmin.value;
-
-    admin = true;
-
-})
-
-document.getElementById('seeReport').addEventListener('click', ()=>
+class Report
 {
 
-    if(admin === false)
-    {    
-        location.href="403.html";
-    }else
+    constructor(btnGuest, btnAdmin, check, seeReport)
     {
-        location.href="report.html";
+
+        this.btnGuest = btnGuest;
+
+        this.btnAdmin = btnAdmin;
+
+        this.check = check;
+
+        this.seeReport = seeReport;
+
+        this.checkAdmin = false;
+
     }
 
-})
+    setGuest()
+    {
+
+        this.btnGuest.addEventListener('click', ()=>
+        {
+
+            this.check.textContent = 'you are logged in as: ' + this.btnGuest.value;
+
+            this.checkAdmin = false;
+
+        })
+
+    }
+
+    setAdmin()
+    {
+
+        this.btnAdmin.addEventListener('click', ()=>
+        {
+
+            this.check.textContent = 'you are logged in as: ' + this.btnAdmin.value;
+            
+            this.checkAdmin = true;
+
+        })
+
+    }
+
+    checkReport()
+    {
+
+        this.setGuest();
+
+        this.setAdmin();
+
+        this.seeReport.addEventListener('click', ()=>
+        {
+
+            if(this.checkAdmin === false)
+            {    
+
+                location.href="403.html";
+
+            }
+            else
+            {
+
+                location.href="report.html";
+
+            }
+
+        })
+
+    }
+
+}
+
+new Report
+(
+
+    document.getElementsByTagName('button')[0],
+
+    document.getElementsByTagName('button')[1],
+    
+    document.getElementsByTagName('p')[0],
+
+    document.getElementById('seeReport'),
+
+).checkReport();
